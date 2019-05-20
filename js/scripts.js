dadf1951f0ddb6de21605de40483a8dffc766119
whatsAJava <jairizy1@yahoo.com>
2019-05-20 11:58:01 -0400

    Bigger sidebar and main text on mobile devices

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,37 @@
 
 // Sidebar Navigation
 
-function openNav() {
-  document.getElementById("mySidenav").style.width = "15.6em";
-  document.getElementById("mySidenav").style.width = "15.6vw";
-  document.getElementById("main").style.marginLeft = "15.6em";
-  document.getElementById("main").style.marginLeft = "15.6vw";
+function openNav(x) {
+  
+	if (x.matches) {
+		document.getElementById("mySidenav").style.width = "30.2em";
+		document.getElementById("mySidenav").style.width = "30.2vw";
+		document.getElementById("main").style.marginLeft = "30.2em";
+		document.getElementById("main").style.marginLeft = "30.2vw";
+		x.addListener(openNav);
+	} else {
+	document.getElementById("mySidenav").style.width = "15.6em";
+	document.getElementById("mySidenav").style.width = "15.6vw";
+	document.getElementById("main").style.marginLeft = "15.6em";
+	document.getElementById("main").style.marginLeft = "15.6vw";
+	}
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
+
+
+/*
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    document.body.style.backgroundColor = "yellow";
+  } else {
+   document.body.style.backgroundColor = "pink";
+  }
+} */
+
+var x = window.matchMedia("(max-width: 450px)")
+// myFunction(x) // Call listener function at run time
+// Attach listener function on state changes
